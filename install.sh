2ecca0cd2d4240b1a0233de7e2061abcf1da5bf3
root <root@arthur.vpn.boap.net>
2014-03-07 21:47:12 +0100

    lol

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ XENSERVER_SUBNET=10.13.1.0/24
 
 sed -i "s/hostname/$HOSTNAME/g" deploy.sh
 mv etc/openvpn/hostname.ovpn etc/openvpn/$HOSTNAME.ovpn
-sed -i "s/addresse_externe_nfs/$NFS_EXTERNAL_ADDR/g" etc/sysconfig/network-scripts/ifcfg-tun1.sh
-sed -i "s/addresse_interne_gre_nfs/$NFS_GRE_ADDR/g" etc/sysconfig/network-scripts/ifcfg-tun1.sh
-sed -i "s/addresse_interne_gre_xenserver/$XENSERVER_GRE_ADDR/g" etc/sysconfig/network-scripts/ifcfg-tun1.sh
+sed -i "s/addresse_externe_nfs/$NFS_EXTERNAL_ADDR/g" etc/sysconfig/network-scripts/ifcfg-tun1
+sed -i "s/addresse_interne_gre_nfs/$NFS_GRE_ADDR/g" etc/sysconfig/network-scripts/ifcfg-tun1
+sed -i "s/addresse_interne_gre_xenserver/$XENSERVER_GRE_ADDR/g" etc/sysconfig/network-scripts/ifcfg-tun1
 sed -i "s/ip_nfs_via_gre/$NFS_GRE_ADDR/g" root/fix_route.sh
 sed -i "s/sous-reseau_xenserver/$XENSERVER_SUBNET/g" root/fix_route.sh
